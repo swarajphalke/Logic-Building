06151ec39ad533f4ab79cf1f58a665f363eca1f8
Swaraj Phalke <swarajphalke555@gmail.com>
2026-03-14 18:49:08 +0530

    Binary Search

---
+++ 02-Array-Programs/02-Array-Programs/BinarySearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Array_Programs
+{
+    internal class BinarySearch
+    {
+        public static int Search(int[] arr, int key)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }else if (arr[mid] < key)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int key = 7;
+            int index = Search(arr, key);
+
+            if(index != -1)
+            {
+                Console.WriteLine("Element found at index: "+ index);
+            }
+            else
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+    }
+}
