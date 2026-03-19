c90412b680d4783720244171895750474737b0fe
Swaraj Phalke <swarajphalke555@gmail.com>
2026-03-19 20:03:52 +0530

    Merge Sorted Array

---
+++ 02-Array-Programs/02-Array-Programs/MergeSortedArrays.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Array_Programs
+{
+    internal class MergeSortedArrays
+    {
+        public static int[] Merge(int[] arr1, int[] arr2)
+        {
+            int[] result = new int[arr1.Length + arr2.Length];
+
+            int i = 0, j = 0, k = 0;
+
+            while (i < arr1.Length && j < arr2.Length)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    result[k++] = arr1[i++];
+                }
+                else
+                {
+                    result[k++] = arr2[j++];
+                }
+            }
+            while (i < arr1.Length)
+            {
+                result[k++] = arr1[i++];
+            }
+            while (j < arr2.Length)
+            {
+                result[k++] = arr2[j++];
+            }
+
+            return result;
+        }
+
+        public static void Main()
+        {
+            int[] arr1 = { 1, 3, 5 };
+            int[] arr2 = { 2, 4, 6 };
+
+            int[] result = Merge(arr1, arr2);
+
+            foreach (int i in result)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
