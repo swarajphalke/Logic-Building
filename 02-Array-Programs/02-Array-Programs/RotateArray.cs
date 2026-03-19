5f3f6d6ef64f494bcce0e9a4a10ecae184a51c2f
Swaraj Phalke <swarajphalke555@gmail.com>
2026-03-19 20:03:44 +0530

    Rotate Array

---
+++ 02-Array-Programs/02-Array-Programs/RotateArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Array_Programs
+{
+    internal class RotateArray
+    {
+        public static void Rotate(int[] nums, int key)
+        {
+            int n = nums.Length;
+            key = key % n;
+
+            Reverse(nums, 0, n - 1);
+            Reverse(nums, 0, key - 1);
+            Reverse(nums, key, n - 1);
+
+            foreach (int i in nums)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        public static void Reverse(int[] nums, int start, int end)
+        {
+            int temp;
+            while (start < end)
+            {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        public static void Main()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int key = 3;
+            Rotate(nums, key);
+        }
+    }
+}
